e72a547205bac461a9c8d239d2dc5c248b925fea
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-01-03 10:16:21 +0530

    Add files via upload

---
+++ 14_Longest_Common_Prefix.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix = strs[0];
+        for (string s : strs)
+            while (s.find(prefix) != 0)
+                prefix = prefix.substr(0, prefix.length() - 1);
+        return prefix;
+    }
+};
