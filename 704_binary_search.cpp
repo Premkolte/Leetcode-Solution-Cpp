fff73c85979baa87a62f764f0440bfe42711cd60
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-01-08 23:24:22 +0530

    Add files via upload

---
+++ 704_binary_search.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+    int n = nums.size();
+    int low = 0 , high = n-1;
+    while(low<=high){
+        int mid = (low +high)/2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid]<target){
+            low = mid + 1;
+        }
+        else{
+            high = mid -1;
+        }
+    }
+    return -1;
+    }
+};
